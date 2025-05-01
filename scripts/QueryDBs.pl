961850f198f707ee2b1314e4b32372410b8b6af5
Walter Lamagna <wlamagna@gmail.com>
2025-05-01 11:07:44 -0300

    Update QueryDBs.pl

--- scripts/QueryDBs.pl
+++ scripts/QueryDBs.pl
@@ -1,1 +1,7 @@
+#!/usr/bin/perl
+# Who has done this before ? Running a perl script from an ADO Pipeline !!!
+# Sure there is someone else that also does this crazy ideas :)
+#
+my $DEBUGFLAG = 1;
+my ($key, $password, $queryFile, $databaseType, $batchSize, $startAtClientId) = @ARGS;
 
