c1c77c6583be8b07d87be092f3305935b748397e
Walter Lamagna <wlamagna@gmail.com>
2025-05-01 11:18:03 -0300

    Update QueryDBs.pl

--- scripts/QueryDBs.pl
+++ scripts/QueryDBs.pl
@@ -18,3 +18,24 @@ print "DEBUG: SQL File: $queryFile\n" if ($DEBUGFLAG);
 print "DEBUG: Database Type: $databaseType\n" if ($DEBUGFLAG);
 print "DEBUG: Batch Size: $batchSize\n" if ($DEBUGFLAG);
 print "DEBUG: OUTPUT: " . $ENV{BUILD_ARTIFACTSTAGINGDIRECTORY} . "\n" if ($DEBUGFLAG);
+
+my $variablesTotal = 2;
+my $LoginPayload = '{
+"svc_id": "a_service_name",
+"password": "' $password . '"
+}';
+
+my $ObtainToken = `curl --silent -X 'POST' "https://non-specific-url.com/api/project/auth/service?"`;
+if $ObtainToken =~ /access_denied/) {
+  print "Failed during the token retrieval - Check credentials";
+  exit;
+}
+
+$ObtainToken =~ s/.*t0k3n":"//g;
+$ObtainToken =~ s/",.*//g;
+
+my $DBPayload_Specific = '{
+"svc_id": "a_database_service",
+"password": "' . $password . '"
+}';
+
