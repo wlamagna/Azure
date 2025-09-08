323bae5402258c3b6edb7c587498ef263cc3175d
Walter Lamagna <wlamagna@gmail.com>
2025-09-08 18:07:38 -0300

    Update getpayloads.ps1

--- getpayloads.ps1
+++ getpayloads.ps1
@@ -8,6 +8,6 @@ foreach ($f in $fils) {
   --container-name thecontainer `
   --name $f `
   --file $f `
-  --account-key '==' `
+  --account-key '<here goes the account key that could come from an AKV>' `
   --auth-mode key
   }
