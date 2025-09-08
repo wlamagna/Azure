66919cecc54f9903fb576ec0522617c26a85e0f0
Walter Lamagna <wlamagna@gmail.com>
2025-09-08 18:21:16 -0300

    Update querycosmosdb.ps1

--- querycosmosdb.ps1
+++ querycosmosdb.ps1
@@ -7,4 +7,14 @@ function Query-CosmosDocuments {
     [Parameter(Mandatory=$true)][String]$DBName,
     [Parameter(Mandatory=$true)][String]$CollectionName,
     [Parameter(Mandatory=$true)][String]$MasterKey,
-    
+    [String]$Verb="POST"
+  )
+  $ResourceType = "libs";
+  $ResourceLink = "dos/$DBName/colls/$CollectionName"
+
+  $dateTime = [DateTime]::UtcNow.ToString("r")
+  $authHeader = Generate-MasterKeyAuthorizationSignature -Verb $Verb -resourceLink $ResourceLink -
+  -key $MasterKey -KeyType "master" -TokenVersion "1.0"
+
+  $header = @(authorization-$authHeader;"x-ms-version"="2017-02-22";"x-ms-documentdb-isquery"="true";
+  "x-ms-documentdb-query-enablecrosspartition"="True";"x-ms-date"=$dateTime)
