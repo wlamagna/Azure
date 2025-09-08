42291de54469b6d0c47a23c627cdc411c6c41afc
Walter Lamagna <wlamagna@gmail.com>
2025-09-08 18:42:42 -0300

    Update querycosmosdb.ps1

--- querycosmosdb.ps1
+++ querycosmosdb.ps1
@@ -18,3 +18,31 @@ function Query-CosmosDocuments {
 
   $header = @(authorization-$authHeader;"x-ms-version"="2017-02-22";"x-ms-documentdb-isquery"="true";
   "x-ms-documentdb-query-enablecrosspartition"="True";"x-ms-date"=$dateTime)
+  $contentType = "application/query+json"
+  $queryUrl = "$Endpoint$ResourceLink/docs"
+
+  [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::T1s12
+
+  Invoke-WebRequest -UseBasicParsing -Uri "$Endpoint/dos/$DBName/colls/productworkservice/docs" `
+  -Method POST `
+  -UserAgent "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/... put here what you need" `
+  -Headers @(
+  "Accept" = "application/json"
+  "Accept-Language" = "es-AR,es;q.... "
+  "Accept-Encoding" = "gzip, defate, bx" 
+  "Referer" = "https://cosmos.azure.com/"
+  "authorization" = $header.authorization
+  "cache-control" = "no-cache"
+  "x-ms-cosmos-allow-tentative-writes" = "true"
+  "x-ms-cosmos-sdk-supportcapabilities" = "1"
+  "x-ms-date" = $dateTime
+  "x-ms-documentdb-isquery" = "true"
+  "x-ms-documentdb-partitionkeyrangeid" = "0"
+  "x-ms-documentdb-populatequerymetrics" = "true"
+  "x-ms-documentdb-query-enable-scan" = "true"
+  "x-ms-documentdb-query-enablecrosspartition" = "true"
+  
+
+
+
+  
