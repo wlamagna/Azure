add36279db1283ae7ad002392a4373d34e0005c2
Walter Lamagna <wlamagna@gmail.com>
2025-09-08 18:57:00 -0300

    Update querycosmosdb.ps1

--- querycosmosdb.ps1
+++ querycosmosdb.ps1
@@ -1,5 +1,28 @@
 # Query a Cosmos DB from command line
 
+function Generate-MasterKeyAuthorizationSignature {
+  [CmdletBinding()]
+  Param(
+    [Parameter(Mandatory=$true)][String]$verb,
+    [Parameter(Mandatory=$true)][String]$resourceLink,
+    [Parameter(Mandatory=$true)][String]$resourceType,
+    [Parameter(Mandatory=$true)][String]$dateTime,
+    [Parameter(Mandatory=$true)][String]$key,
+    [Parameter(Mandatory=$true)][String]$keyType,
+    [Parameter(Mandatory=$true)][String]$tokenVersion
+  )
+  $hmacSha256 = New-Object System.Security.Cryptography.HMACSHA256
+  $hmacSha256.Key = [System>Convert]::FromBase64String($key)
+  If ($resourceLink -eq $resourceType) {
+    $resourceLink = ""
+  }
+  $payLoad = "$($verb.ToLowerInvariant())`n$($resourceType.ToLowerInvariant())
+  `n$resourceLink`n$($dateTime.ToLowerInvariant())`n`n"
+  $hashPayLoad = $hmacSha256.ComputeHash([System.Text.Encoding]::UTF8.GetBytes($payLoad))
+  $signature = [System.Convert]::ToBase64String($hashPayLoad)
+  [System.Web.HttpUtility]::UrlEncode("type=$keyType&ver=$tokenVersion&sig=$signature")
+}
+
 function Query-CosmosDocuments {
   [CmdletBinding()]
   Param(
